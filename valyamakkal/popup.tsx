e10f886b3ad895a73436220a2c0164c518d715d7
Adith Ramdas <aditramdas@gmail.com>
2023-02-20 22:40:28 +0530

    2nd commit

--- valyamakkal/popup.tsx
+++ valyamakkal/popup.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 
+
 function IndexPopup() {
   
 
@@ -8,11 +9,51 @@ function IndexPopup() {
       style={{
         display: "flex",
         flexDirection: "column",
-        padding: 16
+        padding:"0",
+        margin:"0",
+        height:"200px",
+        background:"darkviolet",
+        color:"white"
+        
+        
       }}>
-      <h2>
-        Hello
+      <div style={{
+        display: "",
+        width: "130px",
+        background:"gray",
+        margin:"10px",
+        borderRadius:"15px"
+        
+      }}>
+      <h2 style={{
+        padding:"5px",
+        fontSize: "15px"
+      }}>
+        Hey,Al there?
       </h2>
+      </div>
+      
+      <p style={{
+        display: "flex",
+        flexDirection:"column",
+        justifyContent:"center",
+        alignItems:"center",
+        margin:"0",
+        fontWeight:"500"
+        
+        
+      }}>
+        <p>
+        AI content present:
+        </p>
+
+        <h1 style={{
+        
+      }}>
+          14%
+        </h1>
+         
+      </p>
     </div>
   )
 }
